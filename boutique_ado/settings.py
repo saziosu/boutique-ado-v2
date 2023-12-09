a577240a69261a30069d450d205572d6631d43c1
Sarah O'Sullivan <sarah.osullivan@wpengine.com>
2023-12-09 21:02:55 +0000

    Set up All Auth

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -59,13 +63,34 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',
+                'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by email
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# tells allauth we want to authenticate with emails or usernames
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True  # email is required
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'  # makes email verification required
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True  # makes the user confirm the email address
+ACCOUNT_USERNAME_MIN_LENGTH = 4  # sets the min username length
+LOGIN_URL = '/accounts/login/'  # this is where the user will log in
+LOGIN_REDIRECT_URL = '/'  # this is where the user will be redirected after login
 
 WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
