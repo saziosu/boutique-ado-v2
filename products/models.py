44bcc0d7690e3bf696abc854959a7e635172eb36
Sarah O'Sullivan <sarah.osullivan@wpengine.com>
2023-12-09 21:43:58 +0000

    Added products app, models and fixtures

---
+++ products/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+class Category(models.Model):
+
+    class Meta:
+        # this changes the admin from Categorys to Categories
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    # null and blank to make the friendly name optional
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    # sku = stock keeping unit code
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
