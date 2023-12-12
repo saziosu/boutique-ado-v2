e7159a1753f4aa5658ca8d42b45b5b231b2b50d9
Sarah O'Sullivan <sarah.osullivan@wpengine.com>
2023-12-12 20:28:38 +0000

    Update shopping bag template

--- bag/views.py
+++ bag/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render, redirect
 
+# Create your views here.
+
 def view_bag(request):
-    """
-    a view that renders the bag contents page
-    """
+    """ A view that renders the bag contents page """
+
     return render(request, 'bag/bag.html')
 
 def add_to_bag(request, item_id):
-    """
-    Add a quantity of the specified product to the shopping bag
-    """
+    """ Add a quantity of the specified product to the shopping bag """
+
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
