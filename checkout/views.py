7c22dc734236a9bcc58b72be717389154e78d881
Sarah O'Sullivan <sarah.osullivan@wpengine.com>
2024-01-03 17:55:24 +0000

    Completed webhook handler

--- checkout/views.py
+++ checkout/views.py
@@ -13,10 +13,10 @@ import json
 @require_POST
 def cache_checkout_data(request):
     try:
-        pid = request.POST.get('cient_secret').split('_secret')[0]
+        pid = request.POST.get('client_secret').split('_secret')[0]
         stripe.api_key = settings.STRIPE_SECRET_KEY
         stripe.PaymentIntent.modify(pid, metadata={
-            'bag': json.dump(request.session.get('bag', {})),
+            'bag': json.dumps(request.session.get('bag', {})),
             'save_info': request.POST.get('save_info'),
             'username': request.user,
         })
@@ -25,6 +25,7 @@ def cache_checkout_data(request):
         messages.error(request, 'Sorry, your payment cannot be \
             processed right now. Please try again later.')
         return HttpResponse(content=e, status=400)
+
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
