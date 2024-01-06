c98cb67d38ad8a69f6735821b44c58e658521a0a
Sarah O'Sullivan <sarah.osullivan@wpengine.com>
2024-01-06 17:55:34 +0000

    Added product cuew, url and template for product admin

--- products/views.py
+++ products/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
-from .models import Product, Category
 
+from .models import Product, Category
+from .forms import ProductForm
+
+# Create your views here.
 
 def all_products(request):
-    """
-    view to show all products including sorting and search queries
-    """
+    """ A view to show all products, including sorting and search queries """
+
     products = Product.objects.all()
     query = None
     categories = None
@@ -16,9 +18,7 @@ def all_products(request):
     direction = None
 
     if request.GET:
-
         if 'sort' in request.GET:
-            # sortig the products
             sortkey = request.GET['sort']
             sort = sortkey
             if sortkey == 'name':
@@ -26,20 +26,16 @@ def all_products(request):
                 products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
                 sortkey = 'category__name'
-
             if 'direction' in request.GET:
-                # determine what order to sort these
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
-
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
-
 
         if 'q' in request.GET:
             query = request.GET['q']
@@ -47,15 +43,11 @@ def all_products(request):
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
             
-            # we want to return results if the search is found in either the title or the desc
-            # i makes the queries case insensitive
-            # needs double underscore on the query term
             queries = Q(name__icontains=query) | Q(description__icontains=query)
-            # filter the products by the search query
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
-    
+
     context = {
         'products': products,
         'search_term': query,
@@ -67,9 +59,8 @@ def all_products(request):
 
 
 def product_detail(request, product_id):
-    """
-    view to show individual product details
-    """
+    """ A view to show individual product details """
+
     product = get_object_or_404(Product, pk=product_id)
 
     context = {
@@ -77,3 +68,14 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
