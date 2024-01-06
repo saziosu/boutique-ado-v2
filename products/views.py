a150ed0f95b400b58b807ee5574fac06219bd19f
Sarah O'Sullivan <sarah.osullivan@wpengine.com>
2024-01-06 20:58:33 +0000

    Product admin - editing products

--- products/views.py
+++ products/views.py
@@ -73,17 +73,12 @@ def product_detail(request, product_id):
 def add_product(request):
     """ Add a product to the store """
     if request.method == 'POST':
-        # files to make sure we're capturing the image, if uploaded
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            # if the form is valid, save it
             form.save()
-            # add a success message
             messages.success(request, 'Successfully added product!')
-            # and redirect to the same view
             return redirect(reverse('add_product'))
         else:
-            # any error will result in an error message to check the form details
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
         form = ProductForm()
@@ -91,6 +86,31 @@ def add_product(request):
     template = 'products/add_product.html'
     context = {
         'form': form,
+    }
+
+    return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ Edit a product in the store """
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+    else:
+        form = ProductForm(instance=product)
+        # lets thr user know what product they are editing
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
     }
 
     return render(request, template, context)
