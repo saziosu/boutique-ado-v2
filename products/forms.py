7cdf95162a9d8ee1e393294420a04f186822a1cc
Sarah O'Sullivan <sarah.osullivan@wpengine.com>
2024-01-06 17:19:40 +0000

    Added product form

---
+++ products/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import Product, Category
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        # short hand for loop to set the categories in a list
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        # Update the category names to the friendly names instead of the ID or name field
+        self.fields['category'].choices = friendly_names
+        # iterate through the rest of the fields
+        for field_name, field in self.fields.items():
+            # attach some styling to match our store's theme
+            field.widget.attrs['class'] = 'border-black rounded-0'
